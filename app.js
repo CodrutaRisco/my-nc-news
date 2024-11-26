50e7f6ab38c77bf716a9ae4f8e0ac3e7ed144bd4
CodrutaRisco <codruta.risco@gmail.com>
2024-11-26 15:29:34 +0000

    add /api/articles/:article_id endpoint test 200

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
 const { getApi, getApiTopics } = require("./controllers/api.controllers");
+const { getArticleById } = require("./controllers/article.controllers");
 
 app.get("/api", getApi);
 app.get("/api/topics", getApiTopics);
+app.get("/api/articles/:article_id", getArticleById);
 
+app.use((err, req, res, next) => {
+  console.log(err, "------>err");
+  res.status(500).send({ msg: "******hello" });
+});
 
 module.exports = app;
